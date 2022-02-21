d1b3618cb6ce7be4f11730b6fff6b0b1fbe44e14
ViktoriaMikhaylovskaya <mviktoria065@gmail.com>
2022-02-21 22:10:44 +0300

    Исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-function getRundomNumber (minValue, maxValue) {
-  if (minValue <= 0) {
+function getRandomNumber (minValue, maxValue) {
+  if (minValue < 0) {
     throw new Error('Укажите минимальное значение больше 0');
   }
 
   return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
 }
 
-function getRundomValue (minValue, maxValue, range) {
+getRandomNumber(1, 10);
+
+function getRandomValue (minValue, maxValue, range) {
   if (minValue <= 0) {
     throw new Error('Укажите минимальное значение больше 0');
   }
 
   const rundomNumber = Math.random() * (maxValue - minValue + 1) + minValue;
-  return rundomNumber.toFixed(range);
+  return Number(rundomNumber.toFixed(range));
 }
 
+getRandomValue(1, 5, 2);
