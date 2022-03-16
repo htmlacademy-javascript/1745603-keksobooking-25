6daa0631a049c6377ae9a13ff76146626971976c
ViktoriaMikhaylovskaya <mviktoria065@gmail.com>
2022-03-16 21:39:27 +0300

    Исправляет ошибки

--- js/form.js
+++ js/form.js
@@ -1,34 +1,19 @@
-const formNotis = document.querySelector('.ad-form');
-const fieldFormHeader = document.querySelectorAll('fieldset');
-const mapFilters = document.querySelector('.map__filters');
-const arrSelect = mapFilters.querySelectorAll('select');
+function changePageActitvity(changeToActive) {
+  const formNotis = document.querySelector('.ad-form');
+  const fieldFormHeader = document.querySelectorAll('fieldset');
+  const mapFilters = document.querySelector('.map__filters');
+  const arrSelect = mapFilters.querySelectorAll('select');
 
-function pageUnActive() {
-  formNotis.classList.add('.ad-form--disabled');
-  mapFilters.classList.add('.map__filters--disabled');
+  formNotis.classList[changeToActive ? 'add' : 'remove']('.ad-form--disabled');
+  mapFilters.classList[changeToActive ? 'add' : 'remove']('.map__filters--disabled');
 
   fieldFormHeader.forEach((element) => {
-    element.disabled = true;
+    element.disabled = !changeToActive;
   });
 
   arrSelect.forEach((element) => {
-    element.disabled = true;
+    element.disabled = !changeToActive;
   });
 }
 
-pageUnActive();
-
-function pageActive() {
-  formNotis.classList.remove('.ad-form--disabled');
-  mapFilters.classList.remove('.map__filters--disabled');
-
-  fieldFormHeader.forEach((element) => {
-    element.disabled = false;
-  });
-
-  arrSelect.forEach((element) => {
-    element.disabled = false;
-  });
-}
-
-pageActive();
+export{changePageActitvity};
--- js/main.js
+++ js/main.js
@@ -3,3 +3,6 @@ import './util.js';
 import './create-mock-object.js';
 import './card.js';
 import './form.js';
+import {changePageActitvity} from './form.js';
+
+changePageActitvity(false);
