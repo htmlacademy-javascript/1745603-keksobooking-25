7fcbb9cf9eb9ae505bfa10f87b719515d98ddca2
Keks <keks@htmlacademy.ru>
2022-03-19 18:07:45 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,8 @@
+import './data.js';
+import './util.js';
+import './create-mock-object.js';
+import './card.js';
+import './form.js';
+import {changePageActitvity} from './form.js';
+
+changePageActitvity(true);
